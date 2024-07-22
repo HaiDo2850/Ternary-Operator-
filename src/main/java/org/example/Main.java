914c6ad865b72283fec1106340e65c9d0c79b4d3
Hai <haido246810@gmail.com>
2024-07-22 11:31:32 +0700

    Ternary Operator

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        var exp = 1;
+        var salary = exp > 3 ? 1000 : 500;
+        System.out.println(salary);
+    }
+}
